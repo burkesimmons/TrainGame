36e4def75a2f22a3cf08a38f535a8145fd405c66
Burke Simmons <burkesimmons@gmail.com>
2017-08-05 21:56:02 -0600

    Updated javascript to get moment working

--- assets/javascritp/logic.js
+++ assets/javascritp/logic.js
@@ -12,62 +12,101 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
-var trainName = '';
-var destination = '';
-var nextArrival = '';
-var frequency = '';
+// var trainName = '';
+// var destination = '';
+// var nextArrival = '';
+// var frequency = '';
 
-// var submit = {
-//         name: $('#name-input').val().trim(),
-//         email: $('#email-input').val().trim(),
-//         age: $('#age-input').val().trim(),
-//         comment: $('#comment-input').val().trim(),
-//       }
+
 
 // Capture Button Click
 $("#add-train").on("click", function() {
   // Don't refresh the page!
     event.preventDefault();
+
+    var submit = {
+        trainName: $('#trainName').val().trim(),
+        email: $('#destination').val().trim(),
+        nextArrival: $('#nextArrival').val(),
+        frequency: $('#frequency').val().trim(),
+      }
   
     trainName = $('#trainName').val().trim();
     destination = $('#destination').val().trim();
-    nextArrival = $('#nextArrival').val().trim();
+    nextArrival = $('#nextArrival').val();
     frequency = $('#frequency').val().trim();
 
     database.ref().set({
         trainName: trainName,
         destination: destination,
         nextArrival: nextArrival,
-        frequency: frequency
-    });
-});
+        frequency: frequency,
+    }); 
+    // Assumptions
+    // var frequency = 3;
+    console.log(frequency);
 
+    // Time is 3:30 AM
+    var firstTime = "03:30";
 
-database.ref().on("value", function(snapshot) {
+    // First Time (pushed back 1 year to make sure it comes before current time)
+    var firstTimeConverted = moment(firstTime, "hh:mm").subtract(1, "years");
+    console.log(firstTimeConverted);
 
-  // Print the initial data to the console.
-  console.log(snapshot.val());
+    // Current Time
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
 
-  // Log the value of the various properties
-  console.log(snapshot.val().trainName);
-  console.log(snapshot.val().destination);
-  console.log(snapshot.val().nextArrival);
-  console.log(snapshot.val().frequency);
+    // Difference between the times
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
 
-  // Change the HTML
-  
+    // Time apart (remainder)
+    var tRemainder = diffTime % frequency;
+    console.log(tRemainder);
+
+    // Minute Until Train
+    var tMinutesTillTrain = frequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+    // Next Train
+    nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));   
+
     var table = document.getElementById("tableRow");
     var row = table.insertRow(0);
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
     var cell3 = row.insertCell(2);
     var cell4 = row.insertCell(3);
+    var cell5 = row.insertCell(4);
     // var cell5 = row.insertCell(4);
-    cell1.innerHTML = snapshot.val().trainName;
-    cell2.innerHTML = snapshot.val().destination;
-    cell3.innerHTML = snapshot.val().nextArrival;
-    cell4.innerHTML = snapshot.val().frequency;
+    cell1.innerHTML = trainName;
+    cell2.innerHTML = destination;
+    cell3.innerHTML = frequency;
+    cell4.innerHTML = nextTrain;
+    cell5.innerHTML = tMinutesTillTrain;
     // cell5.innerHTML = "NEW CELL2";
+});
+
+
+database.ref().on("value", function(snapshot) {
+
+  // Print the initial data to the console.
+  // console.log(snapshot.val());
+
+  submit = snapshot.val().newTrain;
+  // Log the value of the various properties
+  // console.log(snapshot.val().trainName);
+  // console.log(snapshot.val().destination);
+  // console.log(snapshot.val().nextArrival);
+  // console.log(snapshot.val().frequency);
+
+  // Change the HTML
+  
+    
+
+
 
 
   // $("#displayed-data").html(snapshot.val().name + " | " + snapshot.val().age + " | " + snapshot.val().phone);
